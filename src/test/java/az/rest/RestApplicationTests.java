39b9805914a4862a956123d91c37d851bc511f81
toshiba <feridxelilov708@gmail.com>
2019-10-27 18:18:38 -0400

    Rest ile bagli her sey burda olcaq

---
+++ src/test/java/az/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package az.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
